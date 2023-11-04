f9f7f80c88503a3b90f0017f75be077b93b99dec
Aibek-Kozonberdiev <ratroniii@gmail.com>
2023-11-04 10:39:52 +0600

    create IsAdminOrReadOnly

---
+++ quiz/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True  # Allowed for GET operation (and other safe methods)
+        return request.user and request.user.is_staff  # Allowed only to administrators for other operations
--- quiz/views.py
+++ quiz/views.py
@@ -2,20 +2,25 @@ from rest_framework import viewsets
 
 from .models import Quiz, Category, Question, Room
 from .serializers import CategorySerializer, QuestionSerializer, QuizSerializer, RoomSerializer
+from .permissions import IsAdminOrReadOnly
 
 
 class ViewSetCategory(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 class ViewSetQuestion(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 class ViewSetQuiz(viewsets.ModelViewSet):
     queryset = Quiz.objects.all().order_by('-create', '-update')
     serializer_class = QuizSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 class ViewSetRoom(viewsets.ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+    permission_classes = [IsAdminOrReadOnly]
